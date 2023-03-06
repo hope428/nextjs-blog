e36a05d6231edf5ea9b6e8fd8b24708f4e5ae313
Leon Forsythe <lvforsythe95@gmail.com>
2023-03-06 14:59:32 -0500

    imported my first image in the nextjs way

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
   return (
+
     <div className={styles.container}>
+
+    <Image 
+      src="/images/profile.JPG.webp"
+      height={144}
+      width={144}
+      alt="Komaeda"
+    />
+
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
